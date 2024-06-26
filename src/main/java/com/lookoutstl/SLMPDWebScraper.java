add51383528e708ca7ab3355a6e391ce1d2173f0
Paul Brockmeyer <brockzilla@gmail.com>
2024-06-26 13:54:28 -0700

    Adding email notifications when parsing fails

--- src/main/java/com/lookoutstl/SLMPDWebScraper.java
+++ src/main/java/com/lookoutstl/SLMPDWebScraper.java
@@ -25,7 +25,9 @@ import org.apache.commons.io.IOUtils;
 public class SLMPDWebScraper {
     private static Logger log = Logger.getLogger(SLMPDWebScraper.class);
 
-    private static final String CALLSFORSERVICE_URL = "http://www.slmpd.org/cfs.aspx";
+    //private static final String CALLSFORSERVICE_URL = "http://www.slmpd.org/cfs.aspx";
+    // They redesigned the site on June 18, 2024
+    private static final String CALLSFORSERVICE_URL = "https://slmpd.org/calls/";
 
     public static Collection<Incident> scrapeIncidents() {
         Collection<Incident> incidents = new ArrayList<Incident>();
@@ -38,6 +40,63 @@ public class SLMPDWebScraper {
             InputStream in = connection.getInputStream();
             String encoding = connection.getContentEncoding();
             encoding = encoding == null ? "UTF-8" : encoding;
+
+            String body = IOUtils.toString(in, encoding);
+            String rowStart = "<tr>";
+            String rowEnd = "</tr>";
+            String columnStart = "<td>";
+            String columnEnd = "</td>";
+
+            while (body.indexOf("<tr>") >= 0) {
+
+                if (body.indexOf(rowStart) > 0) {
+                    body = body.substring(body.indexOf(rowStart) + rowStart.length(), body.length());
+                    String incidentRow = body.substring(0, body.indexOf(rowEnd));
+
+                    String callTimestamp = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    try {
+                        // Sometimes we get junk like 2016-08-03 :2:33 or 2016-08-03 ::34
+                        String[] callTime = callTimestamp.split(" ")[1].split(":");
+                        callTimestamp = callTimestamp.split(" ")[0] + " " +
+                            addZerosIfNecessary(callTime[0]) + ":" +
+                            addZerosIfNecessary(callTime[1]) + ":" +
+                            addZerosIfNecessary(callTime[2]);
+                    } catch (Exception e) {
+                        log.error("Trouble cleaning up timestamp: " + callTimestamp, e);
+                    }
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String id = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String block = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String description = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    description = description.replace("(Specify)", "");
+                    description = description.replace("(specify means)", "");
+                    description = description.replace("(Specify what they are doing)", "");
+                    description = description.replace("Attempt", "Attempted");
+                    description = description.replace("9-1-1", "911");
+                    description = description.replace("Flourishing (Specify Weapon)", "Flourishing Weapon");
+                    description = description.replace("Observation Case", "Observation/Case");
+                    description = description.replace("Larceny", "Larceny/Theft");
+                    description = description.replace("Holding a Person for a ", "Holding Person for ");
+                    description = description.replace("Nude", "Nudity");
+                    description = description.replace("Sundry", "Sundry/Various");
+                    description = description.replace("Accident (Auto Abandoned)", "Accident - Auto Abandoned");
+                    description = description.replace("Shots Fired - Into Dwelling / Property Damage", "Shots Fired into Dwelling");
+                    description = description.replace("Meet an (Officer, Watchman, Car, etc.)", "Meet an Officer/Watchman/Car");
+
+                    description = description.trim();
+
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    incidents.add(new Incident(id, callTimestamp, block, description));
+                }
+            }
+
+            /*
             String body = IOUtils.toString(in, encoding);
             String rowStart = "<tr bgcolor=\"White\">";
             String rowEnd = "</tr>";
@@ -92,6 +151,7 @@ public class SLMPDWebScraper {
                     incidents.add(new Incident(id, callTimestamp, block, description));
                 }
             }
+            */
 
             log.info("Parsed: [" + CALLSFORSERVICE_URL + "] and found " + incidents.size() + " incident/s");
 
