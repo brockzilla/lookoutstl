92cf9309ec957109bfc5c36ad1d5167e522d4d65
Paul Brockmeyer <brockzilla@gmail.com>
2024-06-28 12:26:14 -0700

    Adding email notifications when parsing fails

--- src/main/java/com/lookoutstl/SLMPDWebScraper.java
+++ src/main/java/com/lookoutstl/SLMPDWebScraper.java
@@ -60,61 +60,57 @@ public class SLMPDWebScraper {
 
                 //log.info("Have rows");
 
-                if (body.indexOf(rowStart) > 0) {
+                log.info("Have a row");
 
-                    log.info("Have a row");
+                String incidentRow = body.substring(body.indexOf(rowStart) + rowStart.length(), body.indexOf(rowEnd));
 
-                    body = body.substring(body.indexOf(rowStart) + rowStart.length(), body.length());
-                    String incidentRow = body.substring(0, body.indexOf(rowEnd));
+                if (incidentRow.indexOf(columnStart) >= 0) {
 
-                    if (incidentRow.indexOf(columnStart) >= 0) {
+                    log.info("Have a column");
 
-                        log.info("Have a column");
-
-                        String callTimestamp = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
-                        try {
-                            // Sometimes we get junk like 2016-08-03 :2:33 or 2016-08-03 ::34
-                            String[] callTime = callTimestamp.split(" ")[1].split(":");
-                            callTimestamp = callTimestamp.split(" ")[0] + " " +
-                                addZerosIfNecessary(callTime[0]) + ":" +
-                                addZerosIfNecessary(callTime[1]) + ":" +
-                                addZerosIfNecessary(callTime[2]);
-                        } catch (Exception e) {
-                            log.error("Trouble cleaning up timestamp: " + callTimestamp, e);
-                        }
-                        incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
-
-                        String id = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
-                        incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
-
-                        String block = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
-                        incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
-
-                        String description = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
-                        description = description.replace("(Specify)", "");
-                        description = description.replace("(specify means)", "");
-                        description = description.replace("(Specify what they are doing)", "");
-                        description = description.replace("Attempt", "Attempted");
-                        description = description.replace("9-1-1", "911");
-                        description = description.replace("Flourishing (Specify Weapon)", "Flourishing Weapon");
-                        description = description.replace("Observation Case", "Observation/Case");
-                        description = description.replace("Larceny", "Larceny/Theft");
-                        description = description.replace("Holding a Person for a ", "Holding Person for ");
-                        description = description.replace("Nude", "Nudity");
-                        description = description.replace("Sundry", "Sundry/Various");
-                        description = description.replace("Accident (Auto Abandoned)", "Accident - Auto Abandoned");
-                        description = description.replace("Shots Fired - Into Dwelling / Property Damage", "Shots Fired into Dwelling");
-                        description = description.replace("Meet an (Officer, Watchman, Car, etc.)", "Meet an Officer/Watchman/Car");
-
-                        description = description.trim();
-
-                        log.info("Storing incident: " + id + "|" + callTimestamp + "|" + block + "|" + description);
-
-                        incidents.add(new Incident(id, callTimestamp, block, description));
+                    String callTimestamp = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    try {
+                        // Sometimes we get junk like 2016-08-03 :2:33 or 2016-08-03 ::34
+                        String[] callTime = callTimestamp.split(" ")[1].split(":");
+                        callTimestamp = callTimestamp.split(" ")[0] + " " +
+                            addZerosIfNecessary(callTime[0]) + ":" +
+                            addZerosIfNecessary(callTime[1]) + ":" +
+                            addZerosIfNecessary(callTime[2]);
+                    } catch (Exception e) {
+                        log.error("Trouble cleaning up timestamp: " + callTimestamp, e);
                     }
-
                     incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String id = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String block = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    incidentRow = incidentRow.substring(incidentRow.indexOf(columnEnd) + columnEnd.length(), incidentRow.length());
+
+                    String description = stripHTML(incidentRow.substring(incidentRow.indexOf(columnStart) + columnStart.length(), incidentRow.indexOf(columnEnd)));
+                    description = description.replace("(Specify)", "");
+                    description = description.replace("(specify means)", "");
+                    description = description.replace("(Specify what they are doing)", "");
+                    description = description.replace("Attempt", "Attempted");
+                    description = description.replace("9-1-1", "911");
+                    description = description.replace("Flourishing (Specify Weapon)", "Flourishing Weapon");
+                    description = description.replace("Observation Case", "Observation/Case");
+                    description = description.replace("Larceny", "Larceny/Theft");
+                    description = description.replace("Holding a Person for a ", "Holding Person for ");
+                    description = description.replace("Nude", "Nudity");
+                    description = description.replace("Sundry", "Sundry/Various");
+                    description = description.replace("Accident (Auto Abandoned)", "Accident - Auto Abandoned");
+                    description = description.replace("Shots Fired - Into Dwelling / Property Damage", "Shots Fired into Dwelling");
+                    description = description.replace("Meet an (Officer, Watchman, Car, etc.)", "Meet an Officer/Watchman/Car");
+
+                    description = description.trim();
+
+                    log.info("Storing incident: " + id + "|" + callTimestamp + "|" + block + "|" + description);
+
+                    incidents.add(new Incident(id, callTimestamp, block, description));
                 }
+
+                body = body.substring(body.indexOf(rowEnd) + rowEnd.length(), body.length());
             }
 
             log.info("Parsed: [" + CALLSFORSERVICE_URL + "] and found " + incidents.size() + " incident/s");
