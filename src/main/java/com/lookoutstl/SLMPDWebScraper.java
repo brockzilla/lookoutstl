f9816d6217c25f5963fdce7d0ac73e7f15522b89
Paul Brockmeyer <brockzilla@gmail.com>
2024-06-28 18:20:10 -0700

    Adding email notifications when parsing fails

--- src/main/java/com/lookoutstl/SLMPDWebScraper.java
+++ src/main/java/com/lookoutstl/SLMPDWebScraper.java
@@ -36,6 +36,9 @@ public class SLMPDWebScraper {
 
         HttpURLConnection connection = null;
         try {
+
+
+
             connection = (HttpURLConnection)new URL(CALLSFORSERVICE_URL).openConnection();
             connection.setConnectTimeout(10000); //set timeout to 5 seconds
 
@@ -44,6 +47,15 @@ public class SLMPDWebScraper {
             encoding = encoding == null ? "UTF-8" : encoding;
 
             String body = IOUtils.toString(in, encoding);
+
+
+            connection = (HttpURLConnection)new URL(CALLSFORSERVICE_URL).openConnection();
+            connection.setConnectTimeout(10000); //set timeout to 5 seconds
+            in = connection.getInputStream();
+            encoding = connection.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+            body = IOUtils.toString(in, encoding);
+
 
             String rowStart = "<tr>";
             String rowEnd = "</tr>";
