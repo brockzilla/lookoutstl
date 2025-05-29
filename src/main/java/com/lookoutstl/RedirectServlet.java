f02c16e522c8d2c4be9e74c78c095acf9716a2c2
brockzilla <brockzilla@gmail.com>
2025-05-29 10:18:09 -0700

    Adding support for HTTPS

--- src/main/java/com/lookoutstl/RedirectServlet.java
+++ src/main/java/com/lookoutstl/RedirectServlet.java
@@ -7,8 +7,20 @@ import java.io.*;
 public class RedirectServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uri = request.getRequestURI();
-        if (!uri.startsWith("/.well-known/acme-challenge/")) {
-            response.sendRedirect("http://lookoutstl.com" + uri);
+        String queryString = request.getQueryString();
+        
+        // Don't redirect ACME challenge requests
+        if (uri.startsWith("/.well-known/acme-challenge/")) {
+            return;
+        }
+        
+        // Only redirect HTTPS to HTTP
+        if (request.isSecure()) {
+            String redirectUrl = "http://lookoutstl.com" + uri;
+            if (queryString != null) {
+                redirectUrl += "?" + queryString;
+            }
+            response.sendRedirect(redirectUrl);
         }
     }
 }
